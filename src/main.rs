81e850e3c3edf279b382a549af60569f3d48e3fb
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-16 23:07:01 -0600

    multiple routes

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,23 @@ async fn hello_world() -> &'static str {
     "Hello world"
 }
 
+#[handler]
+async fn service() -> &'static str {
+    "Service"
+}
+
+#[handler]
+async fn about() -> &'static str {
+    "About"
+}
+
 #[tokio::main]
 async fn main() {
-    let router = Router::new().get(hello_world);
+    let router = Router::new()
+        .push(Router::new().path("").get(hello_world))
+        .push(Router::new().path("about").get(about))
+        .push(Router::new().path("service").get(service));
+
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .serve(router)
         .await
